b21a0f19eaa8b2d56c096c23ad3acce2659ca5dc
AmrinShaikh01 <amrins818@gmail.com>
2022-02-21 16:53:25 +0530

    kp-changes

--- github-practice/src/App.js
+++ github-practice/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
-import Testing from "./Components/KHWAJA/Testing";
+import Amrin from "./Components/AMRIN/Amrin";
+import Testing from"./Components/KHWAJA/Testing";
 function App() {
   return (
     <div className="App">
       <Testing />
+     <Amrin/>
     </div>
   );
 }
