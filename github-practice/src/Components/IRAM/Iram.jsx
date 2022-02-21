02609fb9c113e4c50cf257e92c4c4eda2e53a72c
iramnaznin <98594638+iramnaznin@users.noreply.github.com>
2022-02-21 16:52:56 +0530

    kp-created new component

---
+++ github-practice/src/Components/IRAM/Iram.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Iram = () => {
+    return (
+        <div>
+            <h1>Hello EveryOne</h1>
+            <h1>Hello</h1>
+        </div>
+    );
+};
+
+export default Iram;
