136c23097cec8ea229762b9c9c2736a82de15792
Akramdadkhan <97161831+Akramdadkhan@users.noreply.github.com>
2022-02-21 16:58:49 +0530

    Akram new-Component

--- github-practice/src/App.js
+++ github-practice/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Akram from "./Components/AKRAM/Akram";
 import Testing from "./Components/KHWAJA/Testing";
 function App() {
   return (
     <div className="App">
       <Testing />
+      <Akram/>
     </div>
   );
 }
---
+++ github-practice/src/Components/AKRAM/Akram.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Akram = () => {
+    return (
+        <div>
+            <h1>This is Akram Component</h1>
+        </div>
+    );
+};
+
+export default Akram;
