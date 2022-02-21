314d72689a202c2a5bf3a53e0dcd0477f80f02c8
farihakhan72 <97161838+farihakhan72@users.noreply.github.com>
2022-02-21 16:53:48 +0300

    fariha created new comp

--- github-practice/src/App.js
+++ github-practice/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Fariha from "./Components/FARIHA/Fariha";
 import Testing from "./Components/KHWAJA/Testing";
 function App() {
   return (
     <div className="App">
       <Testing />
+      <Fariha />
     </div>
   );
 }
