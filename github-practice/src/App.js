7def21fd71b85ca65d1bc4ac3434bb2c230859e5
AmenaAndlib <97182067+AmenaAndlib@users.noreply.github.com>
2022-02-21 17:03:56 +0530

    Am-cpmment

--- github-practice/src/App.js
+++ github-practice/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Testing from "./Components/KHWAJA/Testing";
+import Amena from "./Components/AMENA/Amena";
 function App() {
   return (
     <div className="App">
       <Testing />
+      <Amena />
     </div>
   );
 }
