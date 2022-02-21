46311792d1b7b3bffd9cbd2d9705322e811b566b
CodeItBest <97164328+CodeItBest@users.noreply.github.com>
2022-02-21 16:00:24 +0530

    amena

--- github-practice/src/App.js
+++ github-practice/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Testing from "./Components/KHWAJA/Testing";
+import Seema from "./Components/SHIRAZSEEMA/Seema";
 function App() {
   return (
     <div className="App">
       <Testing />
+      <Seema/>
     </div>
   );
 }
