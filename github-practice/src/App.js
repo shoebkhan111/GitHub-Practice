882e34d27f003b203bbc46f8b083abe2cafe79c8
WahajAmshan <wahajamshan@gmail.com>
2022-02-21 17:03:18 +0530

    Wahaj Component

--- github-practice/src/App.js
+++ github-practice/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Testing from "./Components/KHWAJA/Testing";
+import Wahaj from "./Components/WAHAJ/Wahaj";
 function App() {
   return (
     <div className="App">
       <Testing />
+      <Wahaj/>
     </div>
   );
 }
---
+++ github-practice/src/Components/WAHAJ/Wahaj.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Wahaj = () => {
+    return (
+        <div>
+            <h1>Hello Wahaj</h1>
+        </div>
+    );
+};
+
+export default Wahaj;
