2d5d744c6d67b0880009f80feee4df7dafac190b
mdazhar1 <mohammadazharshaikh1432@gmail.com>
2022-02-21 18:27:25 +0530

    Azz---hiii

--- github-practice/src/Components/AZHAR/Azhar.jsx
+++ github-practice/src/Components/AZHAR/Azhar.jsx
@@ -6,6 +6,7 @@ const Azhar = () => {
             <h1>My Name is Azhar</h1>
             <h1>hi khwaja sir</h1>
             <h2>city nanded</h2>
+            <h2>add hona be</h2>
         </div>
     );
 };
