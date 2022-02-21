8b124287a9cf1091ed11184ded4db7afd5c15493
mukassar <shaikhmukassar@gmail.com>
2022-02-21 16:54:23 +0530

    SK- Changes in Component

--- github-practice/src/App.js
+++ github-practice/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Testing from "./Components/KHWAJA/Testing";
+import Mukassar from "./Components/MUKASSAR/Mukassar";
 function App() {
   return (
     <div className="App">
       <Testing />
+      <Mukassar/>
     </div>
   );
 }
---
+++ github-practice/src/Components/MUKASSAR/Mukassar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Mukassar = () => {
+    return (
+        <div>
+            <h1>I am Shaikh Mukassar</h1>
+            <p></p>
+        </div>
+    );
+};
+
+export default Mukassar;
