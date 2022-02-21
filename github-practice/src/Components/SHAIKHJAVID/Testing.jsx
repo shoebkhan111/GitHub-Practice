c700e2f86ac5fbe6122d35cb2013925a58b889ed
ShaikhJavid <shaikhjavid06@gmail.com>
2022-02-21 18:50:51 +0530

    SJ-updating PDAC

---
+++ github-practice/src/Components/SHAIKHJAVID/Testing.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Testing = () => {
+    return (
+        <div>
+            <h1>This is Testing Component</h1>
+            <p>This is For You</p>
+           
+        </div>
+    );
+};
+
+export default Testing;
