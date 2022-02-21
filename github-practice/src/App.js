7eada3d4be3ab71ab629de05ed0af407d4921641
ShaikhSameer98 <98589356+ShaikhSameer98@users.noreply.github.com>
2022-02-21 16:55:32 +0530

    S-k New Component created

--- github-practice/src/App.js
+++ github-practice/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Testing from "./Components/KHWAJA/Testing";
+import Sameer from "./Components/Sameer/Sameer";
 function App() {
   return (
     <div className="App">
       <Testing />
+      <Sameer/>
     </div>
   );
 }
---
+++ github-practice/src/Components/Sameer/Sameer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Sameer = () => {
+    return (
+        <div>
+            <h1>Sameer</h1>
+        </div>
+    );
+};
+
+export default Sameer;
