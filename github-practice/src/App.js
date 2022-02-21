ebdec543cac33b40c85ca29ec5768c195d5bea0a
shoebkhan111 <98596262+shoebkhan111@users.noreply.github.com>
2022-02-21 18:23:50 +0530

    SK-Added bew Branch SHOEBKHAN

--- github-practice/src/App.js
+++ github-practice/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Testing from "./Components/KHWAJA/Testing";
-import Shoeb from "./Components/SHOEB/Shoeb";
+// import Shoeb from "./Components/SHOEB/Shoeb";
+import ShoebKhan from "./Components/SHOEBKHAN/ShoebKhan";
 function App() {
   return (
     <div className="App">
       <Testing />
-      <Shoeb/>
+      {/* <Shoeb/> */}
+      <ShoebKhan/>
     </div>
   );
 }
---
+++ github-practice/src/Components/SHOEBKHAN/ShoebKhan.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ShoebKhan = () => {
+    return (
+        <div>
+            <h1>This is ShoebKhan</h1>
+        </div>
+    );
+};
+
+export default ShoebKhan;
