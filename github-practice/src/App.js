68ba51808880a939ce37f8c67e507341fa0f13ea
mdazhar1 <mohammadazharshaikh1432@gmail.com>
2022-02-21 16:53:00 +0530

    kp-new component

--- github-practice/src/App.js
+++ github-practice/src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
 import Testing from "./Components/KHWAJA/Testing";
+import Azhar from "./Components/AZHAR/Azhar"
 function App() {
   return (
     <div className="App">
       <Testing />
+
+
+      
+      <Azhar/>
     </div>
   );
 }
---
+++ github-practice/src/Components/AZHAR/Azhar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Azhar = () => {
+    return (
+        <div>
+            <h1>My Name is Azhar</h1>
+        </div>
+    );
+};
+
+export default Azhar;
