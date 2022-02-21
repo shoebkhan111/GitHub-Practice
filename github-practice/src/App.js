0522aad0b21827bacfdd1836a307f08aa67eb169
Khawja Pasha <khwajapasha141@gmail.com>
2022-02-21 18:15:34 +0530

    KP-Added new branch afroz Sir

--- github-practice/src/App.js
+++ github-practice/src/App.js
@@ -2,21 +2,15 @@ import "./App.css";
 import IrfanShah from "./Components/IRFANSHAH/IrfanShah";
 import Testing from "./Components/KHWAJA/Testing";
 import Amena from "./Components/AMENA/Amena";
+import AfrozSir from "./Components/AFROZ_SIR/AfrozSir";
 function App() {
   return (
     <div className="App">
       <Testing />
- 
-      
-
-      <IrfanShah/>
-    
-    
-    
-    <Amena />
- main
+      <IrfanShah />
+      <Amena />
+      <AfrozSir />
     </div>
   );
 }
-
 export default App;
