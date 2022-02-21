7e71e09a16ec8be38d50af5bf016f0c809ad7b22
Khawja Pasha <khwajapasha141@gmail.com>
2022-02-21 20:31:01 +0530

    KP-Refactor the code

--- github-practice/src/App.js
+++ github-practice/src/App.js
@@ -1,14 +1,9 @@
 import "./App.css";
-
-
 import Akram from "./Components/AKRAM/Akram";
-
 import Fariha from "./Components/FARIHA/Fariha";
-
 import IrfanShah from "./Components/IRFANSHAH/IrfanShah";
-
 import Testing from "./Components/KHWAJA/Testing";
-import Azhar from "./Components/AZHAR/Azhar"
+import Azhar from "./Components/AZHAR/Azhar";
 import Seema from "./Components/SHIRAZSEEMA/Seema";
 import ShoebKhan from "./Components/SHOEBKHAN/ShoebKhan";
 import Sameer from "./Components/Sameer/Sameer";
@@ -16,23 +11,20 @@ import Mukassar from "./Components/MUKASSAR/Mukassar";
 import Amena from "./Components/AMENA/Amena";
 import AfrozSir from "./Components/AFROZ_SIR/AfrozSir";
 
-
 function App() {
   return (
     <div className="App">
       <Testing />
-      
-      <Azhar/>
-      <Akram/>
-      <Seema/>
+      <Azhar />
+      <Akram />
+      <Seema />
       <Fariha />
-       <ShoebKhan/>
-      <Sameer/>
-      <Mukassar/>
+      <ShoebKhan />
+      <Sameer />
+      <Mukassar />
       <IrfanShah />
       <Amena />
       <AfrozSir />
-
     </div>
   );
 }
